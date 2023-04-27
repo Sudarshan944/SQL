169db61eb237d889b867a0009045808e649a78fc
Sudarshan Reddy <88623298+Sudarshan944@users.noreply.github.com>
2023-04-27 13:27:38 +0530

    Add files via upload

---
+++ query 1.sql
@@ -0,0 +1,19 @@
+-- Creating database and its name
+-- create database rgm;
+
+-- create table student(
+-- student_id int primary key not null,
+-- student_name varchar(30) not null,
+-- student_contact int not null,
+-- student_dept varchar(25) not null
+-- );
+SELECT * FROM rgm.student;
+-- INSERT INTO student(student_id,student_name,student_contact,student_dept)
+-- VALUES(101,"SUDARSHAN",12345678,"CSE");
+-- another way
+-- INSERT INTO student VALUES (102,"Nani",015678,"CSE");
+INSERT INTO student VALUES (103,"reddy",032156,"ECE");
+INSERT INTO student VALUES (104,"shanthan",120118,"ECE");
+INSERT INTO student VALUES (105,"dileep",130258,"CE");
+INSERT INTO student VALUES (106,"vinay",140128,"CE");
+INSERT INTO student VALUES (107,"suresh",150128,"CSE"),(108,"charan",160128,"ME");
